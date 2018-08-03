405b93e2938705062cb6633d7831ba311d73c718
Mirv <reski.raptor@gmail.com>
2018-08-03 14:52:29 +0000

    pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,6 +3,7 @@ class Pet < ApplicationRecord
     draft: 5000, published: 200, sold: 4000, cancelled: 5200,
     billing: 20000, ready_for_fulfillment: 20200, fulfilling_processing: 20400,
     removed: 30000, suspended: 30200, banned: 30400 }, _prefix: :pet
+    
   belongs_to :pet_breed
   belongs_to :location
   belongs_to :user
