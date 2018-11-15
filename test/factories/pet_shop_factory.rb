4fa10bb01b8bc4de320ed498116a78502f9d274d
Mirv <reski.raptor@gmail.com>
2018-11-15 19:35:54 +0000

    production secret key

--- test/factories/pet_shop_factory.rb
+++ test/factories/pet_shop_factory.rb
@@ -4,24 +4,25 @@ require 'ffaker'
 FactoryBot.define do
   factory :user do
     email               { FFaker::Internet.email }
-    encrypted_password  "ssssss"
-    password            "ssssss"
-    admin               false
+    encrypted_password  { "ssssss" }
+    password            { "ssssss" }
+    admin               { false }
   end
 
+  # TODO - implement a trait here
   # This will use the User class (Admin would have been guessed)
   factory :admin, class: User do
     email               { FFaker::Internet.email }
-    encrypted_password  "ssssss"
-    password            "ssssss"
-    admin               true
+    encrypted_password  {"ssssss"}
+    password            {"ssssss"}
+    admin               {true}
   end
   
   factory :pet do
     name { FFaker::AnimalUS.common_name }
-    description "MyString"
+    description { "MyString" }
     status { Pet.statuses.values.sample }
-    age 9.99
+    age {9.99}
     location 
     pet_breed
     user
@@ -29,20 +30,20 @@ FactoryBot.define do
   
   factory :location do
     name    { FFaker::Internet.email } # FFaker::Address.city }
-    xcoord  1
-    ycoord  1
+    xcoord  {1}
+    ycoord  {1}
     user
     status { Location.statuses.values.sample }
   end
   
   factory :pet_breed do
     name { FFaker::Food.fruit }
-    description "MyString"
+    description {"MyString"}
     association :pet_category 
   end
   
   factory :pet_category do
     name { FFaker::Food.fruit }
-    description "MyString"
+    description {"MyString"}
   end
 end
